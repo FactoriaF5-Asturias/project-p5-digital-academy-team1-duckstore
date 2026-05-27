e6124128402e37abf97370db8de63c6495c4fcd4
Jennydev-25 <jenny25sr@gmail.com>
2026-05-27 22:39:32 +0200

    fix: add actualizarContador to contacto.js

--- js/contacto.js
+++ js/contacto.js
@@ -1,12 +1,18 @@
-const formulario = document.getElementById('formulario-contacto');
-formulario.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const datosFormulario = {
-        nombre: document.getElementById('nombre').value.trim(),
-        email: document.getElementById('email').value.trim(),
-        mensaje: document.getElementById('mensaje').value.trim(),
-        capturadoEn: new Date().toLocaleString('es-ES')
-    };
-    console.log('Mensaje del formulario recibido:' , datosFormulario);
-    formulario.reset();
+import { actualizarContador } from './carrito.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    actualizarContador();
+
+    const formulario = document.getElementById('formulario-contacto');
+    formulario.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const datosFormulario = {
+            nombre: document.getElementById('nombre').value.trim(),
+            email: document.getElementById('email').value.trim(),
+            mensaje: document.getElementById('mensaje').value.trim(),
+            capturadoEn: new Date().toLocaleString('es-ES')
+        };
+        console.log('Mensaje del formulario recibido:' , datosFormulario);
+        formulario.reset();
+    });
 });
